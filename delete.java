6a9ca1c0cc5b7abfe418c1dd39e211d6c3172625
keerthiram2612 <keerthiram2612@gmail.com>
2022-11-29 19:43:06 +0530

    adding a file to the java file

---
+++ delete.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class delete {
+    public static void main(String args[]){
+    StringBuffer str =new StringBuffer("Hello");
+    str.delete(1,3);
+    System.out.println(str);
+}
+}
---
+++ insert.java
@@ -0,0 +1,11 @@
+import java .io.*;
+public class insert{
+    public static void main(String args[]){
+        StringBuffer str =new StringBuffer("Hello");
+        str.insert(1,"java");
+        System.out.println(str);
+    }
+
+}
+    
+
