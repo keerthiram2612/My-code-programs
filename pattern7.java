81fe01b013a04f039420a916d81a7ae15b672e61
keerthiram2612 <keerthiram2612@gmail.com>
2022-12-05 19:09:39 +0530

    adding the file to the javafile

---
+++ pattern7.java
@@ -0,0 +1,32 @@
+public class pattern7 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.println("*");
+            }
+        for(int j=1;j<=(2*n)-(2*i);j++){
+            System.out.println(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.println("*");
+        }
+        System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+
+            for(int j =1;j<=i;j++){
+                System.out.println("*");
+            }
+        for(int j=1;j<=(2*n)-(2*i);j++){
+            System.out.println(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.println(" ");
+        }
+        System.out.println();
+        }
+
+    }
+} 
+
